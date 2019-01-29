579575405e62fd85d95bfeec1c152ac0e0a312e4
Alejandro <alejandroherpal@gmail.com>
2019-01-29 19:46:11 +0100

    VENGA QUE FUNCIONA HERMANO

--- ProyectoPrueba/src/main/java/com/TotemSoftware/ProyectoPruebaApplication.java
+++ ProyectoPrueba/src/main/java/com/TotemSoftware/ProyectoPruebaApplication.java
@@ -2,7 +2,7 @@ package com.TotemSoftware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//LKSDJFAKLSDJF
 @SpringBootApplication
 public class ProyectoPruebaApplication {
 
